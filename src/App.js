c098a10b7cc5c62ea1f5417a8d272bc80defe8b4
Cosmas Wasswa <cosywas@gmail.com>
2023-10-26 17:33:59 +0300

    Updated app.js to display splash screen

--- src/App.js
+++ src/App.js
@@ -1,8 +1,17 @@
+import React from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import './App.css';
+import Splash from './components/userAccess/splash';
 
 function App() {
   return (
-    <div className="App" />
+    <>
+      <Router>
+        <Routes>
+          <Route path="/" element={<Splash />} />
+        </Routes>
+      </Router>
+    </>
   );
 }
 
