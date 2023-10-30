f636a95ff75229eedb1f2eb9607c2a67d4519494
Cosmas Wasswa <cosywas@gmail.com>
2023-10-30 12:22:10 +0300

    fix errors

--- src/index.js
+++ src/index.js
@@ -1,18 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { Provider } from 'react-redux';
 import './index.css';
 import App from './App';
 import { UserProvider } from './components/userAccess/userContext';
-import store from './redux/store';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <Provider store={store}>
       <UserProvider>
         <App />
       </UserProvider>
-    </Provider>
   </React.StrictMode>,
 );
