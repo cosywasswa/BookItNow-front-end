931707af94f1c1e10b65e0edbb08ee9c49bcf386
Cosmas Wasswa <cosywas@gmail.com>
2023-10-30 11:40:32 +0300

    Added pages for setting up the navigation menu

---
+++ src/components/pages/deleteDoctor.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const DeleteDoctor = () => (
+  <div>deleteDoctor</div>
+);
+
+export default DeleteDoctor;
---
+++ src/components/pages/myReservations.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const MyReservations = () => (
+  <div>myReservations</div>
+);
+
+export default MyReservations;
