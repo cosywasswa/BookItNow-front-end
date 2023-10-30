e4ee31d492b794cd8c28adf99789fc177b20f280
Cosmas Wasswa <cosywas@gmail.com>
2023-10-30 12:11:16 +0300

    Add the provider to the app

--- src/index.js
+++ src/index.js
@@ -1,14 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { Provider } from 'react-redux';
 import './index.css';
 import App from './App';
 import { UserProvider } from './components/userAccess/userContext';
+import store from './redux/store';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <UserProvider>
-      <App />
-    </UserProvider>
+    <Provider store={store}>
+      <UserProvider>
+        <App />
+      </UserProvider>
+    </Provider>
   </React.StrictMode>,
 );
