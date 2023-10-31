50219bd87f44b5e0c349fe7ecdf1bf0452303d54
clin20n3mun <munanaclinton@gmail.com>
2023-10-31 15:36:14 +0200

    Add fetch data to deleteDoctor document

--- src/components/pages/deleteDoctor.js
+++ src/components/pages/deleteDoctor.js
@@ -1,7 +1,27 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import { useSelector, useDispatch } from 'react-redux';
+import { deleteDoctor, fetchDoctors } from '../../redux/doctor/thunk';
 
-const DeleteDoctor = () => (
-  <div>deleteDoctor</div>
-);
+const DeleteDoctor = () => {
+  const { doctors } = useSelector((store) => store.doctor);
+  const dispatch = useDispatch();
+  useEffect(() => {
+    dispatch(fetchDoctors());
+  }, [dispatch]);
+  const handleDelete = (id) => {
+    dispatch(deleteDoctor(id));
+  };
+  return (
+    <div>
+      {doctors.map((doctor) => (
+        <div key={doctor.id}>
+          <h1>{doctor.name}</h1>
+          <button type="button" onClick={() => { handleDelete(doctor.id); }}>Delete</button>
+        </div>
+      ))}
+
+    </div>
+  );
+};
 
 export default DeleteDoctor;
