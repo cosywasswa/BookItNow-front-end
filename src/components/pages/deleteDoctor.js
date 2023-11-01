9d68f0e651f4b97c457f6995cef91ea656baac6f
clin20n3mun <munanaclinton@gmail.com>
2023-11-01 11:26:00 +0200

    Add styles to delete doctor component

--- src/components/pages/deleteDoctor.js
+++ src/components/pages/deleteDoctor.js
@@ -1,5 +1,6 @@
 import React, { useEffect } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
+import { FaTrashCan } from 'react-icons/fa6';
 import { deleteDoctor, fetchDoctors } from '../../redux/doctor/thunk';
 
 const DeleteDoctor = () => {
@@ -12,11 +13,22 @@ const DeleteDoctor = () => {
     dispatch(deleteDoctor(id));
   };
   return (
-    <div>
+
+    <div className="flex h-auto gap-2 flex-col flex justify-center items-center  p-2 mt-25">
+      <h1 className="font-bold text-xl">Delete Doctor</h1>
       {doctors.map((doctor) => (
-        <div key={doctor.id}>
-          <h1>{doctor.name}</h1>
-          <button type="button" onClick={() => { handleDelete(doctor.id); }}>Delete</button>
+        <div key={doctor.id} className="flex w-80 h-30 md:w-3/6 lg:w-2/4 lg:gap-5 border-2 rounded-md gap-2">
+          <img src={doctor.image} alt={doctor.name} className=" w-40  lg:w-52 h-auto image-doctor" />
+          <div className="grid items-start">
+            <div>
+              <p className="lg:text-lg font-bold ">{doctor.name}</p>
+              <p style={{ color: '#c0c2c1' }} className="text-base">{doctor.specialization}</p>
+            </div>
+            <button type="button" className="self-end mb-1 place-self-center justify-self-start border border-red-600 bg-red-600 text-white hover:text-black transition ease-in-out delay-150 bg-red-600  hover:bg-white duration-300  flex items-center p-2 gap-2 rounded-md" onClick={() => { handleDelete(doctor.id); }}>
+              <FaTrashCan />
+              Delete
+            </button>
+          </div>
         </div>
       ))}
 
