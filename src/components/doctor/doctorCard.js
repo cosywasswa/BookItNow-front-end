90f7c89d02557ef010d3cc7a79582c3515f0775f
clin20n3mun <munanaclinton@gmail.com>
2023-11-02 18:48:20 +0200

    Add styles to doctorCard component

--- src/components/doctor/doctorCard.js
+++ src/components/doctor/doctorCard.js
@@ -7,7 +7,7 @@ const DoctorCard = ({
 }) => (
   <>
     <div className="doctor-image">
-      <img src={image} alt={name} className="rounded-full image-doctor w-72 h-72 border-2" />
+      <img src={image} alt={name} className="rounded-full image-doctor w-72 h-72 border-2 hover:brightness-100" />
     </div>
     <p className="text-center">{name}</p>
     <p style={{ color: '#c0c2c1' }} className="text-center">{specialization}</p>
