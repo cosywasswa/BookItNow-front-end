107291247305c49a6565690ae649c9518a0ee53f
clin20n3mun <munanaclinton@gmail.com>
2023-11-02 20:59:14 +0200

    Add styles to deleteDoctor component

--- src/components/pages/deleteDoctor.js
+++ src/components/pages/deleteDoctor.js
@@ -2,7 +2,8 @@ import React from 'react';
 import DeleteDoc from './deleteDoc';
 
 const DeleteDoctor = () => (
-  <div className="flex h-auto gap-2 flex-col flex justify-center mx-auto items-center p-2 mt-25 adjust-width">
+  <div className="flex h-auto gap-3 flex-col flex justify-center mx-auto items-center p-2 mt-20 mb-8 adjust-width">
+    <h1 className="font-bold text-xl">Delete Doctor</h1>
     <DeleteDoc />
   </div>
 );
