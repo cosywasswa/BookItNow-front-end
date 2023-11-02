8521800c1fb77fed1690d4c721a336407b521a0e
clin20n3mun <munanaclinton@gmail.com>
2023-11-02 11:14:35 +0200

    Fix linters jsx-a11y/control-has-associated-label

--- src/components/doctor/swiperButtons.js
+++ src/components/doctor/swiperButtons.js
@@ -6,10 +6,10 @@ const SwiperNavButtons = () => {
   const swiper = useSwiper();
   return (
     <div className="custom-swiper-navigation ">
-      <button type="button" onClick={() => swiper.slidePrev()} className="absolute p-4 bg-gray-200 hover:bg-lime-500 text-white top-1/2 z-10">
+      <button type="button" aria-label="previous slide" onClick={() => swiper.slidePrev()} className="absolute p-4 bg-gray-200 hover:bg-lime-500 text-white top-1/2 z-10">
         <BiLeftArrow />
       </button>
-      <button type="button" onClick={() => swiper.slideNext()} className="absolute p-4 bg-gray-200 hover:bg-lime-500 text-white top-1/2 right-0 z-10">
+      <button type="button" aria-label="forward slide" onClick={() => swiper.slideNext()} className="absolute p-4 bg-gray-200 hover:bg-lime-500 text-white top-1/2 right-0 z-10">
         <BiRightArrow />
       </button>
     </div>
