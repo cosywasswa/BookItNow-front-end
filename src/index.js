e368a7b46aa77b68e3e6f1a547904b82d0282bf3
Cosmas Wasswa <cosywas@gmail.com>
2023-11-03 12:55:43 +0300

    Added the toast container

--- src/index.js
+++ src/index.js
@@ -2,9 +2,11 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import { Provider } from 'react-redux';
+import { ToastContainer } from 'react-toastify';
 import App from './App';
 import store from './redux/store';
 import { UserProvider } from './components/userAccess/userContext';
+import 'react-toastify/dist/ReactToastify.css';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
@@ -12,6 +14,7 @@ root.render(
     <UserProvider>
       <Provider store={store}>
         <App />
+        <ToastContainer />
       </Provider>
     </UserProvider>
   </React.StrictMode>,
