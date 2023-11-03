d79a68f6501cd6b39a65f323521841ee4b05dc9f
clin20n3mun <munanaclinton@gmail.com>
2023-11-03 10:42:23 +0200

    Add pagination on swiper

--- src/components/doctor/doctorlist.js
+++ src/components/doctor/doctorlist.js
@@ -2,9 +2,10 @@ import React, { useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { Link } from 'react-router-dom';
 import { Swiper, SwiperSlide } from 'swiper/react';
-import { Navigation } from 'swiper/modules';
+import { Navigation, Pagination } from 'swiper/modules';
 import 'swiper/css';
 import 'swiper/css/navigation';
+import 'swiper/css/pagination';
 import { fetchDoctors } from '../../redux/doctor/thunk';
 import DoctorCard from './doctorCard';
 import SwiperNavButtons from './swiperButtons';
@@ -25,9 +26,12 @@ const DoctorsList = () => {
       {doctors.length === 0 ? (<h1 className="mx-auto text-xl mt-5 text-slate-500">No Doctor Available!!</h1>)
         : (
           <Swiper
-            modules={[Navigation]}
+            modules={[Navigation, Pagination]}
             spaceBetween={16}
             slidesPerView={1}
+            pagination={{
+              clickable: true,
+            }}
             breakpoints={{
               768: {
                 slidesPerView: 2,
@@ -43,7 +47,7 @@ const DoctorsList = () => {
           >
             {doctors.map((doctor) => (
               <SwiperSlide key={doctor.id} className="px-1">
-                <Link to={`/${doctor.id}`} className="flex flex-col items-center p-2 gap-1 w-4/5 mx-auto hover:border hover:shadow-lg my-1">
+                <Link to={`/${doctor.id}`} className="flex flex-col items-center p-2 gap-1 w-4/5 mx-auto mb-8 hover:border hover:shadow-lg my-1">
                   <DoctorCard
                     name={doctor.name}
                     image={doctor.image}
